e324e27abacd6e6dc7a1d746e9d72623554639f1
zrr <2081964955@qq.com>
2023-04-06 12:39:30 +0800

    feat: login api

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -6,7 +6,7 @@ import { RoleEnum } from '/@/enums/roleEnum';
 import { PageEnum } from '/@/enums/pageEnum';
 import { ROLES_KEY, TOKEN_KEY, USER_INFO_KEY } from '/@/enums/cacheEnum';
 import { getAuthCache, setAuthCache } from '/@/utils/auth';
-import { GetUserInfoModel, LoginParams } from '/@/api/sys/model/userModel';
+import { LoginParams } from '/@/api/sys/model/userModel';
 import { doLogout, getUserInfo, loginApi } from '/@/api/sys/user';
 import { useI18n } from '/@/hooks/web/useI18n';
 import { useMessage } from '/@/hooks/web/useMessage';
@@ -86,7 +86,7 @@ export const useUserStore = defineStore({
         goHome?: boolean;
         mode?: ErrorMessageMode;
       },
-    ): Promise<GetUserInfoModel | null> {
+    ): Promise<UserInfo | null> {
       try {
         const { goHome = true, mode, ...loginParams } = params;
         const data = await loginApi(loginParams, mode);
@@ -99,7 +99,7 @@ export const useUserStore = defineStore({
         return Promise.reject(error);
       }
     },
-    async afterLoginAction(goHome?: boolean): Promise<GetUserInfoModel | null> {
+    async afterLoginAction(goHome?: boolean): Promise<UserInfo | null> {
       if (!this.getToken) return null;
       // get user info
       const userInfo = await this.getUserInfoAction();
@@ -121,7 +121,7 @@ export const useUserStore = defineStore({
       }
       return userInfo;
     },
-    async getUserInfoAction(): Promise<GetUserInfoModel | null> {
+    async getUserInfoAction(): Promise<UserInfo | null> {
       if (!this.getToken) return null;
       const userInfo = await getUserInfo();
       // if (isArray(roles)) {
--- src/views/sys/login/ForgetPasswordForm.vue
+++ src/views/sys/login/ForgetPasswordForm.vue
@@ -1,16 +1,18 @@
 <template>
   <template v-if="getShow">
     <LoginFormTitle class="enter-x" />
-    <Form class="p-4 enter-x" :model="formData" :rules="getFormRules" ref="formRef">
-      <FormItem name="account" class="enter-x">
-        <Input
-          size="large"
-          v-model:value="formData.account"
-          :placeholder="t('sys.login.userName')"
-        />
+    <Form
+      class="p-4 enter-x"
+      :model="formData"
+      :rules="getFormRules"
+      ref="formRef"
+      v-if="!sendSuccess"
+    >
+      <FormItem name="mail" class="enter-x">
+        <Input size="large" v-model:value="formData.mail" :placeholder="t('sys.login.email')" />
       </FormItem>
 
-      <FormItem name="mobile" class="enter-x">
+      <!-- <FormItem name="mobile" class="enter-x">
         <Input size="large" v-model:value="formData.mobile" :placeholder="t('sys.login.mobile')" />
       </FormItem>
       <FormItem name="sms" class="enter-x">
@@ -19,46 +21,85 @@
           v-model:value="formData.sms"
           :placeholder="t('sys.login.smsCode')"
         />
-      </FormItem>
+      </FormItem> -->
 
       <FormItem class="enter-x">
         <Button type="primary" size="large" block @click="handleReset" :loading="loading">
-          {{ t('common.resetText') }}
+          {{ t('sys.login.resetButton') }}
         </Button>
-        <Button size="large" block class="mt-4" @click="handleBackLogin">
+        <!-- <Button size="large" block class="mt-4" @click="handleBackLogin">
           {{ t('sys.login.backSignIn') }}
-        </Button>
+        </Button> -->
       </FormItem>
+      <div class="text-center">
+        <TypographyParagraph type="secondary">
+          {{ t('sys.login.resetDesc1') }}
+        </TypographyParagraph>
+        <TypographyParagraph type="secondary">
+          {{ t('sys.login.resetDesc2') }}
+        </TypographyParagraph>
+      </div>
     </Form>
+
+    <div class="p-4 enter-x" v-else>
+      <Alert type="success" :message="t('sys.login.resetSuccessAlert')" show-icon closable />
+
+      <Button class="my-5" block type="primary" :href="gotoEmailLink" target="__blank">{{
+        t('sys.login.gotoEmail')
+      }}</Button>
+      <Button block :loading="loading" @click="handlerResend">{{
+        t('sys.login.resendEmail')
+      }}</Button>
+    </div>
   </template>
 </template>
 <script lang="ts" setup>
   import { reactive, ref, computed, unref } from 'vue';
   import LoginFormTitle from './LoginFormTitle.vue';
-  import { Form, Input, Button } from 'ant-design-vue';
-  import { CountdownInput } from '/@/components/CountDown';
+  import { Form, Input, Button, TypographyParagraph, Alert } from 'ant-design-vue';
   import { useI18n } from '/@/hooks/web/useI18n';
-  import { useLoginState, useFormRules, LoginStateEnum } from './useLogin';
+  import { useLoginState, useFormRules, LoginStateEnum, useFormValid } from './useLogin';
+  import { forgetPassword } from '/@/api/sys/user';
+  import { gotoEmail } from '/@/utils/email';
 
   const FormItem = Form.Item;
   const { t } = useI18n();
-  const { handleBackLogin, getLoginState } = useLoginState();
+  const { getLoginState } = useLoginState();
   const { getFormRules } = useFormRules();
 
   const formRef = ref();
   const loading = ref(false);
+  const sendSuccess = ref(false);
 
   const formData = reactive({
-    account: '',
-    mobile: '',
-    sms: '',
+    mail: '',
   });
 
+  const { validForm } = useFormValid(formRef);
+
   const getShow = computed(() => unref(getLoginState) === LoginStateEnum.RESET_PASSWORD);
+  const gotoEmailLink = computed(() => 'https://' + gotoEmail(formData.mail));
 
   async function handleReset() {
     const form = unref(formRef);
     if (!form) return;
-    await form.resetFields();
+    const data = await validForm();
+    if (!data) return;
+    try {
+      loading.value = true;
+      await forgetPassword(data);
+      sendSuccess.value = true;
+    } finally {
+      loading.value = false;
+    }
+  }
+
+  async function handlerResend() {
+    try {
+      loading.value = true;
+      await forgetPassword({ ...formData });
+    } finally {
+      loading.value = false;
+    }
   }
 </script>
